54ea2cf0f6e7580a188f2e1ac5973f9992f30e55
annescharf <ascharf@ab.mpg.de>
2022-09-29 23:23:16 +0200

    updated tables including date, indv, and nblocperday

--- Functions_For_Movement_Data/cumulativeDistance.R
+++ Functions_For_Movement_Data/cumulativeDistance.R
@@ -5,15 +5,21 @@ library('lubridate')
 ## saves table per individual called "cumDistDay_MBid_indiv.name.RData", object contained is called "cumDistDay".
 cumulativeDist <-  function(pathToMV, pathToOutputFolder){ 
   moveObj <- readRDS(pathToMV)
+  indiv <- moveObj@idData$individual.local.identifier
+  if(grepl("/", indiv)==T){indiv <- gsub("/","-",indiv)}
+  
   roundTS <- floor_date(timestamps(moveObj), "day") 
+  locPerDayDF <- data.frame(table(roundTS))
+  locPerDayDF$roundTS <- as_datetime(as.character(locPerDayDF$roundTS), tz="UTC", format="%Y-%m-%d")
+  
   moveObjSplitTime <- split(moveObj, roundTS)
   distSplitL <- lapply(moveObjSplitTime, function(x){sum(distance(x))})
   distSplitTab <- do.call("rbind",distSplitL)
-  cumDistDay <- data.frame(commonID=paste(moveObj@idData$study.id, moveObj@idData$individual.local.identifier, row.names(distSplitTab), sep="_"),
-                            cumulativeDist_km=distSplitTab[,1]/1000,
-                            row.names = NULL)
-  # return(cumDistDay)
-  indiv <- moveObj@idData$individual.local.identifier
-  if(grepl("/", indiv)==T){indiv <- gsub("/","-",indiv)}
+  cumDistDay <- data.frame(commonID=paste(moveObj@idData$study.id, indiv, locPerDayDF$roundTS, sep="_"),
+                           individual=indiv,
+                           date=locPerDayDF$roundTS,
+                           locsPerDay=locPerDayDF$Freq,
+                           cumulativeDist_km=distSplitTab[,1]/1000,
+                           row.names = NULL)
   saveRDS(cumDistDay, file=paste0(pathToOutputFolder,"cumDistDay_",moveObj@idData$study.id,"_",indiv,".rds"))
 }
--- Functions_For_Movement_Data/maxNetDisplacement.R
+++ Functions_For_Movement_Data/maxNetDisplacement.R
@@ -6,15 +6,21 @@ library('raster')
 ## saves table per individual called "maxNetDisplDay_MBid_indiv.name.RData", object contained is called "maxNetDisplDay_".
 maxNetDisp <-  function(pathToMV, pathToOutputFolder){ 
   moveObj <- readRDS(pathToMV)
+  indiv <- moveObj@idData$individual.local.identifier
+  if(grepl("/", indiv)==T){indiv <- gsub("/","-",indiv)}
+  
   roundTS <- floor_date(timestamps(moveObj), "day") 
+  locPerDayDF <- data.frame(table(roundTS))
+  locPerDayDF$roundTS <- as_datetime(as.character(locPerDayDF$roundTS), tz="UTC", format="%Y-%m-%d")
+  
   moveObjSplitTime <- split(moveObj, roundTS)
   distSplitL <- lapply(moveObjSplitTime, function(x){max(pointDistance(coordinates(x), lonlat=T, allpairs=T),na.rm=T)})
   distSplitTab <- do.call("rbind",distSplitL)
-  maxNetDisplDay <- data.frame(commonID=paste(moveObj@idData$study.id, moveObj@idData$individual.local.identifier, row.names(distSplitTab), sep="_"),
-                            maxNetDispl=distSplitTab[,1],
-                            row.names = NULL)
-  # return(maxNetDisplDay_)
-  indiv <- moveObj@idData$individual.local.identifier
-  if(grepl("/", indiv)==T){indiv <- gsub("/","-",indiv)}
+  maxNetDisplDay <- data.frame(commonID=paste(moveObj@idData$study.id, indiv, locPerDayDF$roundTS, sep="_"),
+                               individual=indiv,
+                               date=locPerDayDF$roundTS,
+                               locsPerDay=locPerDayDF$Freq,
+                               maxNetDispl_km=distSplitTab[,1]/1000,
+                               row.names = NULL)
   saveRDS(maxNetDisplDay, file=paste0(pathToOutputFolder,"maxNetDisplDay_",moveObj@idData$study.id,"_",indiv,".rds"))
 }
