f909b571bb4194efa23a5deae6c23affefb2bf61
annescharf <ascharf@ab.mpg.de>
2022-10-06 14:12:32 +0200

    how get weighted grace per day per indiv

---
+++ M7_weightedGRACE_per_dailyDBB.R
@@ -0,0 +1,4 @@
+### to get the weighted daily GRACE per indiv, 
+# 1- annotate the "dailyDBBcoordinatesSPDF_.......rds" with grace, 
+# 2- than multiply the extracted grace value by the column "layer", 
+# 3- and sum it up per day -> this value is the weighted mean grace per day
