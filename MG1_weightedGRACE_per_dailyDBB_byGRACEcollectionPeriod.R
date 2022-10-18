bdeb5928a42bddedfed231e1f7557861fb8612d6
mscacco <martina.scacco@gmail.com>
2022-10-18 16:17:20 +0200

    Building GAMs

--- MG1_weightedGRACE_per_dailyDBB_byGRACEcollectionPeriod.R
+++ MG1_weightedGRACE_per_dailyDBB_byGRACEcollectionPeriod.R
@@ -19,77 +19,105 @@ atlGrace_br@z <- atlGrace_infoBr@z
 # Import table with the GRACE collection periods
 graceTimes <- readRDS("RemoteSensingData/grace_tws_collectionPeriods.rds")
 graceTimes <- graceTimes[order(graceTimes$grace_time),]
+graceTimes$grace_seq <- 1:nrow(graceTimes)
 # List individual files containing infos of daily UDs
-uds_indDay <- list.files("MovementData/UDs/nonFlying_dBB_spdf", full.names = T)
+uds_indDay <- list.files("MovementData/MovementMetrics/nonFlying_dBB_spdf", full.names = T)
 
 # Create directory to store result
-dir.create("MovementData/UDs/nonFlying_dBB_graceExperienced")
+dir.create("MovementData/MovementMetrics/nonFlying_dBB_graceExperienced")
 
 
 #_______________
 ## step 1 and 2: extract values from the correct grace layer and multiply it by the dbb value per pixel
 
-lapply(uds_indDay[1:2], function(fl){
+ud_grace_fls <- list.files("MovementData/MovementMetrics/nonFlying_dBB_graceExperienced", full.names = T)
+flsToDo <- uds_indDay[!gsub(".*SPDF_|.rds","",uds_indDay) %in% gsub(".*grace_|.rds","",ud_grace_fls)]
+
+results <- lapply(flsToDo, function(fl)try({
   print(fl)
   ud <- readRDS(fl)
-  # add common id and rename dbb layer ## NOT NECESSARY ANYMORE, ADDED commonID AND dBBvalue WHEN CREATING THE SPDF
-  # MBid_indiv <- gsub(".*dailyDBBcoordinatesSPDF_|.rds","",fl)
-  # ud$commonID <- paste(MBid_indiv, as.character(ud$date), sep="_")
-  # names(ud)[names(ud)=="layer"] <- "dbb_val" # NOW: "dBBvalue" # I CHENGED IT BELOW
+  MBid_indiv <- gsub(".*dailyDBBcoordinatesSPDF_|.rds","",fl)
   # add empty columns for grace times
-  ud@data[,c("grace_layer","start_graceCollection","end_graceCollection")] <- as.character(NA)
+  ud@data[,c("grace_layer","start_graceCollection","end_graceCollection","grace_seq")] <- as.character(NA)
   
   # subset the ud dataset to only the time range in which GRACE was collected
   #range(ud@data$date); min(graceTimes$start_collection); max(graceTimes$end_collection)
   ud <- ud[ud@data$date >= min(graceTimes$start_collection) & 
-                 ud@data$date <= max(graceTimes$end_collection),]
-  
-  # assign each daily ud (each row) to the grace layer-collection it belongs to
-  for(i in 1:nrow(ud)){
-    minTimeDiff <- which.min(abs(difftime(ud$date[i], graceTimes$grace_time, units="days")))
-    closerGrace <- graceTimes[c(minTimeDiff-1,minTimeDiff,minTimeDiff+1),]
-    whichCollection <- ud$date[i] >= closerGrace$start_collection & 
-      ud$date[i] <= closerGrace$end_collection
-    # if a pixel/day is close to a layer but it's not comprised in the collection period the step is skipped (no grace layer is associated to that day in the UD dataframe)
-    # otherwise the grace collection period including the day will be associated to the UD dataframe
-    # should there be more than one layer to associate we select the first one (by adding [1,] to the closerGrace selection)
-    if(any(whichCollection[!is.na(whichCollection)]==T)){
-      ud[i,c("grace_layer","start_graceCollection","end_graceCollection")] <- as.matrix(closerGrace[whichCollection,c("grace_time","start_collection","end_collection")][1,])
-    }
+             ud@data$date <= max(graceTimes$end_collection),]
+  if(nrow(ud)>0){
+    # for days within the collection period difference in time = 0 and inCollectionnPeriod="yes", 
+    # for days outside the collection period we will measure time difference in days and inCollectionnPeriod="no"
+    ud$timeDiff_daysFromCollection <- 0
+    ud$inCollectionPeriod <- "yes"
+    
+    for(i in 1:nrow(ud)){
+      minTimeDiff <- which.min(abs(difftime(ud$date[i], graceTimes$grace_time, units="days")))
+      closerGrace <- graceTimes[minTimeDiff,]
+      # if the daily ud is not comprised in the collection period of the closest grace layer
+      # a time difference in days from the start/end of the closest collection period is included in the dataframe
+      if(ud$date[i] < closerGrace$start_collection | 
+         ud$date[i] > closerGrace$end_collection){
+        ud$timeDiff_daysFromCollection[i] <- min(abs(difftime(ud$date[i], 
+                                                              c(closerGrace$start_collection,closerGrace$end_collection), units="days")))
+        ud$inCollectionPeriod[i] <- "no"
+      }
+      # otherwise the time difference stays as 0.
+      # This way all observation dates are always associated to a grace layer (even if farther away in time) 
+      ud[i,c("grace_layer","start_graceCollection","end_graceCollection","grace_seq")] <- as.matrix(closerGrace[,c("grace_time","start_collection","end_collection","grace_seq")])
+    }  
+    # there should be no NAs because all ud days should be associated to one grace layer
+    print(table(is.na(ud@data$grace_layer)))
+    print(table(ud$inCollectionPeriod))
+    # For the next step we need the raster brick with all the GRACE layers, we will extract values from the layer corresponding to each day
+    # step 1: DBB pixels in days that could not be associated to any grace layer are automatically excluded from the output dataframe
+    ud_graceLs <- split(ud, ud$grace_layer)
+    
+    ud_grace <- as.data.frame(rbindlist(lapply(ud_graceLs, function(ud_gr){
+      graceLayer <- atlGrace_br[[which(atlGrace_br@z$Date == unique(ud_gr$grace_layer))]]
+      ud_gr$grace_tws <- extract(graceLayer, ud_gr, method="bilinear") 
+      return(as.data.frame(ud_gr))
+    })))
+    # step 2: multiply the grace value by the dBBvalue (occurrence) to obtain the grace experienced in each DBB pixel
+    ud_grace$graceExperienced <- ud_grace$dBBvalue * ud_grace$grace_tws
+    
+    # save output (still multiple rows/pixels per day)
+    saveRDS(ud_grace, file=paste0("MovementData/MovementMetrics/nonFlying_dBB_graceExperienced/dailyDBBgrace_",MBid_indiv,".rds"))
   }
-  ud@data$start_graceCollection <- as.Date(ud@data$start_graceCollection,"%Y-%m-%d")
-  ud@data$end_graceCollection <- as.Date(ud@data$end_graceCollection,"%Y-%m-%d")
-  # check that all days assign to a grace layers are actually included in the data collection period
-  print(all(ud@data$date > ud@data$start_graceCollection & 
-              ud@data$date < ud@data$end_graceCollection, na.rm=T))
-  
-  # For the next step we need the raster brick with all the GRACE layers, we will extract values from the layer corresponding to each day
-  # step 1: DBB pixels in days that could not be associated to any grace layer are automatically excluded from the output dataframe
-  ud_graceLs <- split(ud, ud$grace_layer)
-  
-  ud_grace <- as.data.frame(rbindlist(lapply(ud_graceLs, function(ud_gr){
-    graceLayer <- atlGrace_br[[which(atlGrace_br@z$Date == unique(ud_gr$grace_layer))]]
-    ud_gr$grace_tws <- extract(graceLayer, ud_gr, method="bilinear") 
-    return(as.data.frame(ud_gr))
-  })))
-  # step 2: multiply the grace value by the dBBvalue (occurrence) to obtain the grace experienced in each DBB pixel
-  ud_grace$graceExperienced <- ud_grace$dBBvalue * ud_grace$grace_tws
-  
-  # save output (still multiple rows/pixels per day)
-  saveRDS(ud_grace, file=paste0("MovementData/UDs/nonFlying_dBB_graceExperienced/dailyDBBgrace_",MBid_indiv,".rds"))
-})
+}))
+## Check reasons for errors:
+is.error <- function(x) inherits(x, "try-error")
+table(vapply(results, is.error, logical(1)))
+# no errors, the movement metrics were simply outside the grace collection periods
 
+
+##old script that would exclude days that are outside the collection period:
+# # assign each daily ud (each row) to the grace layer-collection it belongs to
+# for(i in 1:nrow(ud)){
+#   minTimeDiff <- which.min(abs(difftime(ud$date[i], graceTimes$grace_time, units="days")))
+#   closerGrace <- graceTimes[c(minTimeDiff-1,minTimeDiff,minTimeDiff+1),]
+#   whichCollection <- ud$date[i] >= closerGrace$start_collection & 
+#     ud$date[i] <= closerGrace$end_collection
+#   # if a pixel/day is close to a layer but it's not comprised in the collection period the step is skipped (no grace layer is associated to that day in the UD dataframe)
+#   # otherwise the grace collection period including the day will be associated to the UD dataframe
+#   # should there be more than one layer to associate we select the first one (by adding [1,] to the closerGrace selection)
+#   if(any(whichCollection[!is.na(whichCollection)]==T)){
+#     ud[i,c("grace_layer","start_graceCollection","end_graceCollection")] <- as.matrix(closerGrace[whichCollection,c("grace_time","start_collection","end_collection")][1,])
+#   }
+# }
+# check that all days assign to a grace layers are actually included in the data collection period
+# print(all(ud@data$date > ud@data$start_graceCollection & 
+#             ud@data$date < ud@data$end_graceCollection, na.rm=T))
 
 #___________
 ## step 3: sum the grace experienced in each DBB pixel per day, to obtain one value of grace experienced per day
 
 # List files with grace experienced per DBB pixel per day
-ud_grace_fls <- list.files("MovementData/UDs/nonFlying_dBB_graceExperienced", full.names = T)
+ud_grace_fls <- list.files("MovementData/MovementMetrics/nonFlying_dBB_graceExperienced", full.names = T)
 
 all_indivDailyGraceLS <- lapply(ud_grace_fls, function(f)try({
   udg <- readRDS(f)
-  udg_day <- cbind(aggregate(cbind(dBBvalue, graceExperienced) ~ commonID + date, data=udg, sum),
-        grace_layer=aggregate(grace_layer ~ commonID, data=udg, unique)[,2])
+  udg_day <- cbind(aggregate(cbind(dBBvalue, graceExperienced) ~ commonID + date, data=udg, FUN=sum, na.action=na.pass),
+                  aggregate(cbind(grace_layer,start_graceCollection,end_graceCollection,grace_seq,timeDiff_daysFromCollection,inCollectionPeriod) ~ commonID + date, data=udg, FUN=unique, na.action=na.pass)[,-(1:2)])
   return(udg_day)
 }))
 
@@ -97,13 +125,12 @@ is.error <- function(x) inherits(x, "try-error")
 ## Check reasons for errors:
 table(vapply(all_indivDailyGraceLS, is.error, logical(1)))
 # which returned errors/messages and why? by assigning names (seq_along) we can remove list elements (the ones which did not return errors) but we maintain the original list indexing
-if(any(vapply(all_indivDailyGraceLS, is.error, logical(1)))){
 names(all_indivDailyGraceLS) <- seq_along(all_indivDailyGraceLS)
-all_indivDailyGraceLS <- all_indivDailyGraceLS[!vapply(all_indivDailyGraceLS, is.error, logical(1))]
-}
+all_indivDailyGraceLS[vapply(all_indivDailyGraceLS, is.error, logical(1))]
+
 # rbind the list and save
 all_indivDailyGraceExperienced <- as.data.frame(rbindlist(all_indivDailyGraceLS))
-saveRDS(all_indivDailyGraceExperienced, file="MovementData/UDs/nonFlying_allDailyGraceExperienced.rds")
+saveRDS(all_indivDailyGraceExperienced, file="MovementData/MovementMetrics/nonFlying_allDailyGraceExperienced.rds")
 
 
 
--- MG2_averageByGracePeriods_buildModelDF.R
+++ MG2_averageByGracePeriods_buildModelDF.R
@@ -2,32 +2,109 @@
 # Bring in the table with the calculated metrics per day, merge the information and average them by the grace collection periods
 # Use this to build a dataset and run the following models:
 #_______________
-# 1- monthly average of daily UD size ~ monthly grace experienced (calculated by multiplying UD probabilities * monthly grace values) - we expect larger UDs in poorer conditions
-# 2- monthly average of daily cumulative distance ~ monthly grace experienced (calculated as above) - higher daily distance in poorer conditions
-# 3- monthly "directness" of movement within a pixel ~ monthly grace raw values per pixel (pixel based, we could calculate directness by averaging the DBB variance per segment by month and pixel) - we expect more directed movement (lower dbb variance) in poor quality pixels
+# 1- monthly average of daily UD size ~ monthly grace experienced (calculated by multiplying UD probabilities * monthly grace values) - we expect larger UDs in poorer conditions (negative grace) (neg. corr)
+# 2- monthly average of daily cumulative distance ~ monthly grace experienced (calculated as above) - higher daily distance in poorer conditions (negative grace) (neg. corr)
+# 3- monthly "directness" of movement ~ monthly grace experienced (calculated as above) - we expect more directed movement (lower dbb variance) in poorer conditions (negative grace) (pos. corr)
 #_______________
 
 setwd("/home/mscacco/ownCloud/Martina/ProgettiVari/GRACE")
 
-# Import table with the GRACE collection periods
-graceTimes <- readRDS("RemoteSensingData/grace_tws_collectionPeriods.rds")
-graceTimes <- graceTimes[order(graceTimes$grace_time),]
+#___________________________________
+# CREATE A DATAFRAME FOR THE MODELS
+
 # Import table with metrics and grace experienced per day and per individual
-daily_graceExp <- readRDS("MovementData/UDs/nonFlying_allDailyGraceExperienced.rds")
-daily_metrics <- readRDS("MovementData/UDs/nonFlying_UDcalculations.rds")
-# and the reference table including individual infos and locomotory mode
-indivInfos <- readRDS()
+daily_graceExp <- readRDS("MovementData/MovementMetrics/nonFlying_allDailyGraceExperienced.rds")
+# and the table including individual infos, locomotory mode, daily movement metrics and ud size
+daily_metrics <- readRDS("MovementData/MovementMetrics/nonFlying_metrics.rds")
 
-# Create a column containing MBid and Indiv
-daily_graceExp$MBid_indiv <- sapply(lapply(strsplit(daily_graceExp$commonID,"_"),"[",1:2),paste,collapse="_")
-# add the start and end of the grace collection periods
-collectionPeriods <- data.frame(grace_layer=as.character(graceTimes$grace_time),
-                                collPeriod=paste(graceTimes$start_collection, graceTimes$end_collection, sep="_"))
-all(unique(daily_graceExp$grace_layer) %in% collectionPeriods$grace_layer)
-daily_graceExp <- merge(x=daily_graceExp, y=collectionPeriods, 
-                        by="grace_layer", all.x=T) 
+# merge the two daily tables
+dailyDF <- merge(daily_metrics, daily_graceExp[,-2], by="commonID", all=T)
+# Create two variables for aggregation, one pasting MBid_individual and one pasting start and end of the grace collection period
+#MBid_indiv <- sapply(lapply(strsplit(daily_graceExp$commonID,"_"),"[",1:2),paste,collapse="_")
+MBid_indiv <- paste(dailyDF$MBid, dailyDF$individual, sep="_")
+graceCollPeriod <- paste(dailyDF$start_graceCollection, dailyDF$end_graceCollection, sep="_")
 
-# average variables by collection period
-modelDF <- aggregate(cbind(graceExperienced) ~ MBid_indiv + collPeriod, data=daily_graceExp, mean)
-  
-  
+# Average variables by collection period (more or less monthly)
+
+modelDF <- aggregate(cbind(UDsizeKm2,cumulativeDist_km,motionVariance,graceExperienced,UDwMeanLongitude,UDwMeanLatitude) 
+                     ~ MBid_indiv + graceCollPeriod + species + Locomotion + grace_layer + grace_seq,
+                     data=dailyDF, FUN=mean)
+modelDF$grace_seq <- as.numeric(modelDF$grace_seq)
+modelDF$species <- as.factor(modelDF$species)
+
+#________
+# MODELS
+
+library(mgcv)
+
+# udMod <- gamm(log(UDsizeKm2) ~
+#                 graceExperienced +
+#                 s(UDwMeanLongitude,UDwMeanLatitude) + #(weighted coordinates from dayilyUD)
+#                 s(grace_seq), #(time, since first measurement of Grace)
+#                 #Locomotion #(locomotory.mode)
+#                 random = list(species=~1), #(random fac)
+#               data=modelDF,
+#               na.action=na.fail) #by default
+# 
+# summary(udMod$gam)
+# hist(residuals(udMod$gam))
+# ## Extract random structure
+# summary(udMod$lme)
+# #intercept for each species
+# udMod$lme$coefficients$random$species 
+# # standard deviation of species intercepts (this is what is printed in the summary)
+# RIsd=0.90
+
+# or, similar:
+udMod <- gam(log(UDsizeKm2) ~
+                s(graceExperienced) +
+                s(UDwMeanLongitude,UDwMeanLatitude) + #(weighted coordinates from dayilyUD)
+                s(grace_seq) + #(time, since first measurement of Grace)
+              #Locomotion #(locomotory.mode)
+               s(species, bs="re"), #(random fac)
+              data=modelDF,
+             na.action=na.fail) #by default
+summary(udMod)
+hist(residuals(udMod))
+
+
+distMod <- gam(log(cumulativeDist_km) ~
+                s(graceExperienced) +
+                s(UDwMeanLongitude,UDwMeanLatitude) + #(weighted coordinates from dayilyUD)
+                s(grace_seq) + #(time, since first measurement of Grace)
+                #Locomotion #(locomotory.mode)
+                s(species, bs="re"), #(random fac)
+              data=modelDF)
+summary(distMod)
+hist(residuals(distMod))
+
+
+dirMod <- gam(log(motionVariance) ~
+                s(graceExperienced) +
+                s(UDwMeanLongitude,UDwMeanLatitude) + #(weighted coordinates from dayilyUD)
+                s(grace_seq) + #(time, since first measurement of Grace)
+                #Locomotion #(locomotory.mode)
+                s(species, bs="re"), #(random fac)
+              data=modelDF)
+summary(dirMod)
+hist(residuals(dirMod))
+
+# diagnostic plots
+par(mfrow=c(2,2))
+gam.check(udMod) 
+gam.check(distMod) 
+gam.check(dirMod) 
+# temporal autocorrelation
+par(mfrow=c(3,1))
+acf(resid(udMod)) 
+acf(resid(distMod))
+acf(resid(dirMod))
+# partial effect plot (smooth terms)
+# vis.gam(udMod,view=c("graceExperienced","grace_seq"), type="response",plot.type="contour")
+# vis.gam(distMod,view=c("graceExperienced","grace_seq"), type="response",plot.type="contour")
+# vis.gam(dirMod,view=c("graceExperienced","grace_seq"), type="response",plot.type="contour")
+plot.gam(udMod, select=1, shade=T)
+plot.gam(distMod, select=1, shade=T)
+plot.gam(dirMod, select=1, shade=T)
+
+
