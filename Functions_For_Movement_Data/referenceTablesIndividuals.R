566287c55cae4a7fc3247e886f00819fceb20cc4
annescharf <ascharf@ab.mpg.de>
2022-10-27 09:39:05 +0200

    update of the reference table per individual

--- Functions_For_Movement_Data/referenceTablesIndividuals.R
+++ Functions_For_Movement_Data/referenceTablesIndividuals.R
@@ -4,33 +4,45 @@ library('lubridate')
 ## this function gives one table per individual, with:
 # - MBid, individual,  species, date #tag,
 # - tracking_duration_in_days, GPSpts_total
-# - GPSSpts_day, median_timelag_mins_day
+# - locsPerDay, median_timelag_mins_day
+# - locomotion mode, and adds genus name of some that did not include it in MB
 ## output is "RefTableIndiv_MBid_indiv.name.rds"
 
-# pathToMV <-  "/home/anne/Documents/GRACEdata/MoveObjects_1hour_noOutliers//10006517_996-92355.rds"    
+# pathToMV <-  "/home/anne/Documents/GRACEdata/MoveObjects_1hour_noOutliers//10135184_Megan.rds"
+# flsMV <- list.files("/home/anne/Documents/GRACEdata/MoveObjects_1hour_noOutliers/", pattern="10135184", full.names = T)
+# moveObj <- readRDS(flsMV[1])     
 
-referenceTable_Individuals <-  function(pathToMV,pathToOutputFolder){ 
+referenceTable_Individuals <-  function(pathToMV, pathToOutputFolder,walkingGenus,swimmingGenus,flyingGenus,snakeGenus,missSps){ 
   moveObj <- readRDS(pathToMV)
   indiv <- moveObj@idData$individual.local.identifier
   if(grepl("/", indiv)==T){indiv <- gsub("/","-",indiv)}
+  # if(grepl(" ", indiv)==T){indiv <- gsub(" ","",indiv)}
   
   RefTableIndiv0 <- data.frame(
     commonID = paste0(moveObj@idData$study.id,"_",indiv,"_",floor_date(timestamps(moveObj), "day")),
-    MBid = moveObj@idData$study.id,
+    MBid = as.character(moveObj@idData$study.id),
     individual = indiv,
     # tag.local.identifier = if(is.null(moveObj@idData$tag.local.identifier)){moveObj$tag.local.identifier},
     # deployment.id = moveObj@idData$deployment.id, ## check what happens when indiv has 2 several deployments
     species= moveObj@idData$individual.taxon.canonical.name,
+    genus=  sub(" .*", "", moveObj@idData$individual.taxon.canonical.name),
     date = floor_date(timestamps(moveObj), "day"),
     tracking_duration_days = as.numeric(round(difftime(timestamps(moveObj)[n.locs(moveObj)], timestamps(moveObj)[1], "days"))),
     GPSpts_total = n.locs(moveObj)
   )
   RefTableIndiv <- RefTableIndiv0[!duplicated(RefTableIndiv0), ]
   
+  ## to add missing sps and genus. "missSps" table comes from here: "/home/anne/Documents/GRACEdata/studiesWithMissingSpsName_completed.csv"
+  if(unique(RefTableIndiv$MBid) %in% missSps$MBid){
+    RefTableIndiv$species <- missSps$species[missSps$MBid==unique(RefTableIndiv$MBid)]
+    RefTableIndiv$genus <- missSps$genus[missSps$MBid==unique(RefTableIndiv$MBid)]
+  }
+  ##
+  
   roundTS <- floor_date(timestamps(moveObj), "day") 
   moveObjSplitTime <- split(moveObj, roundTS)
   
-  RefTableIndiv$GPSpts_day <- unlist(lapply(moveObjSplitTime, n.locs))
+  RefTableIndiv$locsPerDay <- unlist(lapply(moveObjSplitTime, n.locs))
   
   medTL <- unlist(
     lapply(moveObjSplitTime, function(x){
@@ -39,6 +51,20 @@ referenceTable_Individuals <-  function(pathToMV,pathToOutputFolder){
       return(medianTL)
     }))
   RefTableIndiv$median_timelag_mins_day <- round(medTL)
+  
+  if(is.na(unique(RefTableIndiv$genus))){RefTableIndiv$Locomotion <- "unknownSps"} else {
+    if(unique(RefTableIndiv$genus)%in%c("Animalia", "Aves")){RefTableIndiv$Locomotion <- "unknownSps"}
+    if(unique(RefTableIndiv$species)%in%c("Rhincodon typus", "Physeter macrocephalus", "Eretmochelys imbricata", "Balaenoptera musculus", "Balaenoptera physalus")){RefTableIndiv$Locomotion <- "excluded"}
+    if(unique(RefTableIndiv$genus)=="Homo"){RefTableIndiv$Locomotion <- "excluded"}
+    if(unique(RefTableIndiv$genus) %in% walkingGenus){RefTableIndiv$Locomotion <- "walking"}
+    if(unique(RefTableIndiv$genus) %in% swimmingGenus){RefTableIndiv$Locomotion <- "swimming"}
+    if(unique(RefTableIndiv$genus) %in% flyingGenus){RefTableIndiv$Locomotion <- "flying"}
+    if(unique(RefTableIndiv$genus) %in% snakeGenus){RefTableIndiv$Locomotion <- "snake"}
+    if(is.null(RefTableIndiv$Locomotion)){stop(paste0("'",unique(RefTableIndiv$genus),"':"," not included in locomotion lists"))}
+    # if(is.null(RefTableIndiv$Locomotion)){
+    #   missgenus <- unique(RefTableIndiv$genus)
+    #   return(missgenus)} # this was to find the missing genus
+  }
   
   saveRDS(RefTableIndiv, file=paste0(pathToOutputFolder,"RefTableIndiv_",moveObj@idData$study.id,"_",indiv,".rds"))
 }
--- M5_IndivRefTable_addLocomotryMode.R
+++ M5_IndivRefTable_addLocomotryMode.R
@@ -1,3 +1,41 @@
+###############
+## IUCN ## getting the sps lists out of the shp
+#############
+library(rgdal)
+walkMam <- readOGR("/home/anne/Documents/GRACEdata/IUCNshp/WalkingMammals/", "data_0")
+head(walkMam)
+walkMamSps <- unique(walkMam$BINOMIAL)
+saveRDS(walkMamSps, file=paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","WalkingMammals",".rds"))
+rm(walkMam)
+#----#
+swimMam <- readOGR("/home/anne/Documents/GRACEdata/IUCNshp/SwimingMammals/", "data_0")
+head(swimMam@data)
+swimMamSps <- unique(swimMam$BINOMIAL)
+saveRDS(swimMamSps, file=paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","SwimingMammals",".rds"))
+rm(swimMam)
+#----#
+flyMam <- readOGR("/home/anne/Documents/GRACEdata/IUCNshp/FlyingMammals/", "data_0")
+head(flyMam@data)
+flyMamSps <- unique(flyMam$BINOMIAL)
+saveRDS(flyMamSps, file=paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","FlyingMammals",".rds"))
+rm(flyMam)
+#----#
+nonPasser <- readOGR("/home/anne/Documents/GRACEdata/IUCNshp/NonPasserine/", "data_0")
+head(nonPasser@data)
+nonPasserSps <- unique(nonPasser$BINOMIAL)
+saveRDS(nonPasserSps, file=paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","NonPasserine",".rds"))
+rm(nonPasser)
+#----#
+passer <- readOGR("/home/anne/Documents/GRACEdata/IUCNshp/Passerine/", "data_0")
+head(passer@data)
+passerSps <- unique(passer$BINOMIAL)
+saveRDS(passerSps, file=paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","Passerine",".rds"))
+rm(passer)
+
+###########################################
+### creating individual reference table ### final version of this is at the end of the script ~L150!!!
+###########################################
+
 dir.create("/home/anne/Documents/GRACEdata/ReferenceTables/") #only once
 
 library(doParallel)
@@ -12,20 +50,145 @@ is.error <- function(x) inherits(x, "try-error")
 flsMV <- list.files("/home/anne/Documents/GRACEdata/MoveObjects_1hour_noOutliers/", full.names = T)
 pathToOutputFolder <- "/home/anne/Documents/GRACEdata/ReferenceTables/"
 
+walkMamSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","WalkingMammals",".rds"))
+swimMamSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","SwimingMammals",".rds"))
+flyMamSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","FlyingMammals",".rds"))
+nonPasserSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","NonPasserine",".rds"))
+passerSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","Passerine",".rds"))
+walkingGenus <- c(unique(sub(" .*", "", walkMamSps)),"Homo","Chelonoidis","Chelonia","Varanus","Testudinidae","Kinosternon","Taurotragus") #Chelonia
+swimmingGenus <- c(unique(sub(" .*", "", swimMamSps)),"Rhincodon","Eretmochelys")
+flying <- c(flyMamSps,nonPasserSps,passerSps)
+flyingGenus <- c(unique(sub(" .*", "", flying)),"Pandion","Pagophila","Alca","Aves","Cairina","Ichthyaetus","Ardeidae","Anseriformes","Neophron","Gruidae") #Aves,Anseriformes
+snakeGenus <- c("Boiga")
+
+
 start_time <- Sys.time()
-results <- llply(flsMV, function(f)try({referenceTable_Individuals(f, pathToOutputFolder)})
+results <- llply(flsMV, function(f)try({referenceTable_Individuals(f, pathToOutputFolder,walkingGenus,swimmingGenus,flyingGenus,snakeGenus)})
+                 ,.parallel = T)
+# results2 <- llply(failed, function(f)try({referenceTable_Individuals(f, pathToOutputFolder,walkingGenus,swimmingGenus,flyingGenus,snakeGenus)})
+# ,.parallel = T)
+
+
+saveRDS(results, file=paste0("/home/anne/Documents/GRACEdata/","ReferenceTables_listWerros",".rds"))
+
+end_time <- Sys.time()
+end_time - start_time #15mis
+
+table(vapply(results, is.error, logical(1))) # Check potential errors:
+names(results) <- seq_along(results)
+results[vapply(results, is.error, logical(1))]
+flsMV[vapply(results, is.error, logical(1))]
+# results <- readRDS(paste0("/home/anne/Documents/GRACEdata/","ReferenceTables_listWerros",".rds"))
+failed <- flsMV[vapply(results, is.error, logical(1))]
+
+# resultsSPS <- unique(unlist(results[unlist(lapply(results, is.null)==FALSE)])) ## to find out which genus were not included in IUCN
+
+
+## checking results for the ones with error in 1st round
+table(vapply(results2, is.error, logical(1))) # Check potential errors:
+names(results2) <- seq_along(results2)
+results2[vapply(results2, is.error, logical(1))]
+rL <- results2[vapply(results2, is.error, logical(1))]
+
+lapply(1:length(rL), function(x) rL[[x]][1])
+
+failed2 <- failed[vapply(results2, is.error, logical(1))]
+
+#############################################################
+### checking table, to evaluate missing info like sps etc ###
+#############################################################
+
+flsReftInd <- list.files("/home/anne/Documents/GRACEdata/ReferenceTables/", full.names = T)
+refIndvL <- lapply(flsReftInd, function(x){
+  readRDS(x)
+})
+refIndvDF <- data.table::rbindlist(refIndvL)  #dplyr::bind_rows(refIndvL) 
+head(refIndvDF)
+
+# refIndvDF[refIndvDF$Locomotion=="unknownSps" & !is.na(refIndvDF$genus),]
+length(unique(refIndvDF$MBid[refIndvDF$Locomotion=="unknownSps" & is.na(refIndvDF$genus)]))
+missingsps <- data.frame(MBid=unique(refIndvDF$MBid[refIndvDF$Locomotion=="unknownSps" & is.na(refIndvDF$genus) | refIndvDF$genus%in%c("Chelonia", "Aves", "Anseriformes")]))
+write.csv(missingsps,file="/home/anne/Documents/GRACEdata/studiesWithMissingSpsName.csv", row.names = F)
+
+
+sort(table(refIndvDF$genus),decreasing=T)
+# Ciconia,Eidolon, Gruidae, Gyps, 
+# Aquila, Buteo, Milvus
+# remove: "Rhincodon typus, Physeter macrocephalus, Eretmochelys imbricata, Balaenoptera musculus, Balaenoptera physalus, Homo, Animalia, Aves,
+# run walking and swiming UDcode
+
+unique(refIndvDF$MBid[refIndvDF$Locomotion=="swimming"])
+unique(refIndvDF$species[refIndvDF$Locomotion=="swimming"])
+
+unique(refIndvDF$MBid[refIndvDF$genus=="Animalia" & !is.na(refIndvDF$genus)])
+
+####################################################################
+## checking table with manually searched info about sps and genus ##
+####################################################################
+misSps <- read.csv("/home/anne/Documents/GRACEdata/studiesWithMissingSpsName_MS.csv")
+flsMV <- list.files("/home/anne/Documents/GRACEdata/MoveObjects_1hour_noOutliers/", pattern="2082834654", full.names = T)
+
+tbL <- lapply(flsMV, function(x){
+  mv <- readRDS(x)
+  data.frame(indv=mv@idData$individual.local.identifier, sps=mv@idData$individual.taxon.canonical.name)
+})
+
+dplyr::bind_rows(tbL) 
+
+
+misSpsIncl <- misSps[!misSps$comments%in%c("taxa was available in Movebak","gps calibration data","taxa was available in Movebak (In a first phase the focus will be on the Blackbird, but other thrush species will be included as well)","taxa was available in Movebak, tracking domestic ducks: approach for documenting poultry market chains"),]
+misSpsIncl <- misSpsIncl[!misSpsIncl$genus=="",]
+
+write.csv(misSpsIncl, "/home/anne/Documents/GRACEdata/studiesWithMissingSpsName_completed.csv", row.names = F)
+
+misSpsIncl$genus[!misSpsIncl$genus %in% c(walkingGenus,swimmingGenus,flyingGenus)]
+
+
+
+###########################################
+### creating individual reference table ### final version!!!
+###########################################
+
+dir.create("/home/anne/Documents/GRACEdata/ReferenceTables/") #only once
+
+library(doParallel)
+library(plyr)
+mycores <- detectCores()-1 # always make sure to not use all cores of your PC, at least leave 1 free
+registerDoParallel(mycores) 
+
+source("Functions_For_Movement_Data/referenceTablesIndividuals.R")
+
+is.error <- function(x) inherits(x, "try-error")
+
+flsMV <- list.files("/home/anne/Documents/GRACEdata/MoveObjects_1hour_noOutliers/", full.names = T)
+pathToOutputFolder <- "/home/anne/Documents/GRACEdata/ReferenceTables/"
+
+walkMamSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","WalkingMammals",".rds"))
+swimMamSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","SwimingMammals",".rds"))
+flyMamSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","FlyingMammals",".rds"))
+nonPasserSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","NonPasserine",".rds"))
+passerSps <- readRDS(paste0("/home/anne/Documents/GRACEdata/IUCNshp/SpsRDS/","Passerine",".rds"))
+walkingGenus <- c(unique(sub(" .*", "", walkMamSps)),"Homo","Chelonoidis","Chelonia","Varanus","Testudinidae","Kinosternon","Taurotragus","Terrapene") #Chelonia
+swimmingGenus <- c(unique(sub(" .*", "", swimMamSps)),"Rhincodon","Eretmochelys")
+flying <- c(flyMamSps,nonPasserSps,passerSps)
+flyingGenus <- c(unique(sub(" .*", "", flying)),"Pandion","Pagophila","Alca","Aves","Cairina","Ichthyaetus","Ardeidae","Anseriformes","Neophron","Gruidae") #Aves,Anseriformes
+snakeGenus <- c("Boiga")
+
+missSps <- read.csv("/home/anne/Documents/GRACEdata/studiesWithMissingSpsName_completed.csv")
+
+
+start_time <- Sys.time()
+results <- llply(flsMV, function(f)try({referenceTable_Individuals(f, pathToOutputFolder,walkingGenus,swimmingGenus,flyingGenus,snakeGenus,missSps)})
                  ,.parallel = T)
 
 saveRDS(results, file=paste0("/home/anne/Documents/GRACEdata/","ReferenceTables_listWerros",".rds"))
 
 end_time <- Sys.time()
-end_time - start_time #17mis
+end_time - start_time #15mis
 
 table(vapply(results, is.error, logical(1))) # Check potential errors:
 names(results) <- seq_along(results)
 results[vapply(results, is.error, logical(1))]
-
+flsMV[vapply(results, is.error, logical(1))]
 # results <- readRDS(paste0("/home/anne/Documents/GRACEdata/","ReferenceTables_listWerros",".rds"))
-# failed <- flsMV[vapply(results, is.error, logical(1))]
-# 
-# failed[vapply(results, is.error, logical(1))]
+failed <- flsMV[vapply(results, is.error, logical(1))]
